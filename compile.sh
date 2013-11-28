72e4728d66d2e43ecccee75df5385dd641dd0e23
Sebastian Pape <github@pape.io>
2013-11-28 16:33:53 +0100

    setup

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#haml ./example/src/example.haml ./example/example.html && 
+#sass ./src/css/ambilight-table.sass ./lib/css/ambilight-table.css && 
+coffee -cj ./preChange-event.js ./src/js/preChange-event.coffee #&& 
+#coffee --output ./example/ --compile ./example/src/example.coffee && 
+#sass ./example/src/example.sass ./example/example.css
+exit
